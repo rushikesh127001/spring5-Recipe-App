4904d60589a41f95e6844ce4dbccaec6851335b5
rushikesh127001 <chasrushikesh@gmail.com>
2022-01-26 15:48:57 +0530

    initial commit

---
+++ src/test/java/learning/spring/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.spring.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
