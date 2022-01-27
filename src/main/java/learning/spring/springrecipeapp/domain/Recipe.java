36397de9417f0ae6aac1e34dae70b57deeb0fe17
rushikesh127001 <chasrushikesh@gmail.com>
2022-01-27 17:10:51 +0530

    Established 1ToMany relationshup between ingredtiends and Recipe

--- src/main/java/learning/spring/springrecipeapp/domain/Recipe.java
+++ src/main/java/learning/spring/springrecipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package learning.spring.springrecipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -15,6 +16,9 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String directions;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
     @Lob
     private Byte[] image;
 
